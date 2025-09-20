ce0edcfeab1aac0025e3a1a918cee494eb2afbd4
Jim O'Flaherty <jim.oflaherty.jr@qalocate.com>
2025-09-20 11:53:27 -0500

    Added IntegerOps without tests

---
+++ src/main/java/org/java_underling/lang/IntegerOps.java
@@ -0,0 +1,54 @@
+package org.java_underling.lang;
+
+import org.java_underling.util.ArraysOps;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+//TODO: add javadocs
+public class IntegerOps {
+
+  public static int[] findSetBitIndices(int bits) {
+    if (bits == 0) {
+      return ArraysOps.EMPTY_INT_ARRAY;
+    }
+
+    int[] bitsIndices = new int[32];
+    int size = 0;
+    while (bits != 0) {
+      int c = bits & -bits;
+      int index = Integer.numberOfTrailingZeros(c);
+      bitsIndices[size++] = index;
+      bits = bits ^ c;
+    }
+    var result = new int[size];
+    System.arraycopy(bitsIndices, 0, result, 0, size);
+
+    return result;
+  }
+
+  @NotNull
+  public static List<Integer> findSetBitIndicesAsList(int bits) {
+    if (bits == 0) {
+      return List.of();
+    }
+
+    return Arrays.stream(findSetBitIndices(bits))
+        .boxed()
+        .toList();
+  }
+
+  @NotNull
+  public static Set<Integer> findSetBitIndicesAsSet(int bits) {
+    if (bits == 0) {
+      return Set.of();
+    }
+
+    return Arrays.stream(findSetBitIndices(bits))
+        .boxed()
+        .collect(Collectors.toUnmodifiableSet());
+  }
+}
